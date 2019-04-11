d1b410daaf093d9e570944af5823db3ec99d04ad
经理 <boss-hj@163.com>
2019-04-11 17:17:10 +0800

    我帮你加了循环操作，加鸡腿！

--- project/aa.py
+++ project/aa.py
@@ -1,8 +1,12 @@
 def main():
-    num1 = int(input("请输入第一个数字："))
-    num2 = int(input("请输入第二个数字："))
-    sum = num1 + num2
-    print("努力运算中...sum:", sum)
+    while True:
+        i = input("运算开始0结束，1开始：")
+        if i == "0":
+            break
+        num1 = int(input("请输入第一个数字："))
+        num2 = int(input("请输入第二个数字："))
+        sum = num1 + num2
+        print("努力运算中...sum:", sum)
 
 
 if __name__ == '__main__':
