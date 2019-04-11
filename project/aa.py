2df55241dae26da1fa850a984350604f28d2a638
胡健 <hjj0828@163.com>
2019-04-11 19:29:44 +0800

    新人初到，多多指教！

--- project/aa.py
+++ project/aa.py
@@ -4,11 +4,19 @@ def main():
         if i == "0":
             break
         num1 = int(input("请输入第一个数字："))
+        operator = input("请输入+、-、*、/：")
         num2 = int(input("请输入第二个数字："))
-        sum = num1 + num2
-        print("努力运算中...sum:", sum)
+        if operator == "+":
+            print(f"你进行了加发运算，拼命运算中...{num1 + num2}")
+        elif operator == "-":
+            print(f"你进行了减法运算，拼命运算中...{num1 - num2}")
+        elif operator == "*":
+            print(f"你进行了乘法运算，拼命运算中...{num1 * num2}")
+        elif operator == "/":
+            print(f"你进行了除法运算，拼命运算中...{num1 / num2}")
+        else:
+            print("请好好输入运算符~")
 
 
 if __name__ == '__main__':
     main()
-
