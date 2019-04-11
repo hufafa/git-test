cb4f045b7ce92cde2928d676f323727e537c8a4e
胡发发 <hufafa@163.com>
2019-04-11 17:00:44 +0800

    简单的加法运算demo

---
+++ project/aa.py
@@ -0,0 +1,10 @@
+def main():
+    num1 = int(input("请输入第一个数字："))
+    num2 = int(input("请输入第二个数字："))
+    sum = num1 + num2
+    print("努力运算中...sum:", sum)
+
+
+if __name__ == '__main__':
+    main()
+
